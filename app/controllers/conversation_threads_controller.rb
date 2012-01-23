6e1dec521a3088b90ec658bf284804a278dfabad
Duncan Shingleton <duncan@shingleton.org>
2012-01-23 15:59:44 +0000

    Chatbot integration

--- app/controllers/conversation_threads_controller.rb
+++ app/controllers/conversation_threads_controller.rb
@@ -9,26 +9,26 @@ class ConversationThreadsController < ApplicationController
     user_id = params[:conversation_thread][:user_id]
     content = params[:conversation_thread][:content]
     
-    logger.debug "conversation attributes hash: #{conversation_id}"
-
-    
     if conversation_id == "false"
       conversation = Conversation.new(:micropost_id => micropost_id, :user_id => user_id)
       if conversation.save
-       # conversation_id = Conversation.where("micropost_id = ? AND user_id = ?", micropost_id, user_id)
-       conversation_id = conversation.id
+        conversation_id = conversation.id
       end
     end 
     
-     conversation_thread = ConversationThread.new(:conversation_id => conversation_id, :user_id => user_id, :content => content)
-      if conversation_thread.save
-        flash[:success] = "Conversation created"
-      
-      else
-        flash[:error] = "There has been a problem creating your conversation"
-      end
-
+    conversation_thread = ConversationThread.new(:conversation_id => conversation_id, :user_id => user_id, :content => content)
     
+    if conversation_thread.save
+      flash[:success] = "Conversation created"
+      if TuringUser.find_by_id(Micropost.find_by_id(micropost_id).user.turing_user_id).name == "Twitbot"
+        user_id = Micropost.find_by_id(micropost_id).user_id
+        content = conversation_thread.chatbot_reply(conversation_id, content)
+        conversation_thread = ConversationThread.new(:conversation_id => conversation_id, :user_id => user_id, :content => content)
+        conversation_thread.save
+      end
+    else
+      flash[:error] = "There has been a problem creating your conversation"
+    end
     
     redirect_to request.referer
   end
